0c4d92c680fef7eed85688f885d1c605c09c6faf
Rohan Suradkar <rohansuradkar01@gmail.com>
2026-03-28 19:09:11 +0530

    Update client url for production enviroment

--- ExamNotesAI/server/index.js
+++ ExamNotesAI/server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://examnotesaiclient-h07k.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
